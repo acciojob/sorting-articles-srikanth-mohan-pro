6c8159ebfc75c5ca8820df635c4293e4ead84d70
Srikanth Mohan <srikanth.mohan.98@gmail.com>
2026-02-12 19:48:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const bands=[
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog'
+];
+
+function strip(name){
+	return name.replace(/^(a |an |the )/i,"");
+}
+
+bands.sort((a,b)=>strip(a).localeCompare(strip(b)));
+const ul=document.getElementById("band");
+bands.forEach(band=>{
+	const li=document.createElement("li");
+	li.textContent=band;
+	ul.appendChild(li);
+});
+
+
+
+
+
